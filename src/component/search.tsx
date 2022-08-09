08d753978c31292ecdb97d8457888e40e1fb8293
Armin <ArminSheng@users.noreply.github.com>
2022-08-09 10:34:08 +0800

    Update search.tsx

--- src/component/search.tsx
+++ src/component/search.tsx
@@ -4,6 +4,6 @@ import { IconSearch } from "./icons"
 export const Search = (props: { onChange?: ChangeEventHandler<HTMLInputElement> }) => {
     return <div className="search-input-wrap">
         <IconSearch />
-        <input className="search-input" placeholder="plase input" {...props} />
+        <input className="search-input" placeholder="please input" {...props} />
     </div>
 }
